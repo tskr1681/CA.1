d96bbf97549f16729e0bd8882756340b91213ce4
ca_warmerdam <c.a.warmerdam@st.hanze.nl>
2018-11-29 23:35:33 +0100

    Added a logger

--- src/main/java/nl/bioinf/cawarmerdam/compound_evolver/model/CallablePipelineContainer.java
+++ src/main/java/nl/bioinf/cawarmerdam/compound_evolver/model/CallablePipelineContainer.java
@@ -3,8 +3,10 @@ package nl.bioinf.cawarmerdam.compound_evolver.model;
 import chemaxon.marvin.plugin.PluginException;
 
 import java.util.concurrent.Callable;
+import java.util.logging.Logger;
 
 public class CallablePipelineContainer implements Callable<Void> {
+    private static final Logger LOGGER = Logger.getLogger(CallablePipelineContainer.class.getName());
     private PipelineStep<Candidate, Void> pipeline;
     private Candidate candidate;
 
