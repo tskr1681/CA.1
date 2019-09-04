39177632c51ce6e7c8971a441e398d04d113dc32
Hylke Middel <h.middel@st.hanze.nl>
2019-09-04 14:17:32 +0200

    Significantly simplified the NumberCheckUtilities class by making use of builtins.

--- src/main/java/nl/bioinf/cawarmerdam/compound_evolver/util/NumberCheckUtilities.java
+++ src/main/java/nl/bioinf/cawarmerdam/compound_evolver/util/NumberCheckUtilities.java
@@ -4,8 +4,6 @@
  */
 package nl.bioinf.cawarmerdam.compound_evolver.util;
 
-import java.util.regex.Pattern;
-
 /**
  * Class that has utilities for checking if a string can be parsed to an integer.
  *
@@ -23,15 +21,13 @@ public class NumberCheckUtilities {
      * @return true if the string is an integer.
      */
     public static boolean isInteger(String string, int radix) {
-        if (string.isEmpty()) return false;
-        for (int i = 0; i < string.length(); i++) {
-            if (i == 0 && string.charAt(i) == '-') {
-                if (string.length() == 1) return false;
-                else continue;
-            }
-            if (Character.digit(string.charAt(i), radix) < 0) return false;
+        try {
+            System.out.println("string = " + string);
+            Integer.parseInt(string, radix);
+            return true;
+        } catch (Exception e) {
+            return false;
         }
-        return true;
     }
 
     /**
@@ -41,45 +37,11 @@ public class NumberCheckUtilities {
      * @return true if the string can be a double, false if not.
      */
     public static Boolean isDouble(String parameter) {
-        final String Digits = "(\\p{Digit}+)";
-        final String HexDigits = "(\\p{XDigit}+)";
-        // an exponent is 'e' or 'E' followed by an optionally
-        // signed decimal integer.
-        final String Exp = "[eE][+-]?" + Digits;
-        final String fpRegex = (
-                "[\\x00-\\x20]*" +  // Optional leading "whitespace"
-                        "[+-]?(" + // Optional sign character
-                        "NaN|" +           // "NaN" string
-                        "Infinity|" +      // "Infinity" string
-
-                        // A decimal floating-point string representing a finite positive
-                        // number without a leading sign has at most five basic pieces:
-                        // Digits . Digits ExponentPart FloatTypeSuffix
-                        //
-                        // Since this method allows integer-only strings as input
-                        // in addition to strings of floating-point literals, the
-                        // two sub-patterns below are simplifications of the grammar
-                        // productions from section 3.10.2 of
-                        // The Java Language Specification.
-
-                        // Digits ._opt Digits_opt ExponentPart_opt FloatTypeSuffix_opt
-                        "(((" + Digits + "(\\.)?(" + Digits + "?)(" + Exp + ")?)|" +
-
-                        // . Digits ExponentPart_opt FloatTypeSuffix_opt
-                        "(\\.(" + Digits + ")(" + Exp + ")?)|" +
-
-                        // Hexadecimal strings
-                        "((" +
-                        // 0[xX] HexDigits ._opt BinaryExponent FloatTypeSuffix_opt
-                        "(0[xX]" + HexDigits + "(\\.)?)|" +
-
-                        // 0[xX] HexDigits_opt . HexDigits BinaryExponent FloatTypeSuffix_opt
-                        "(0[xX]" + HexDigits + "?(\\.)" + HexDigits + ")" +
-
-                        ")[pP][+-]?" + Digits + "))" +
-                        "[fFdD]?))" +
-                        "[\\x00-\\x20]*");// Optional trailing "whitespace"
-
-        return Pattern.matches(fpRegex, parameter);
+        try {
+            Double.parseDouble(parameter);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
