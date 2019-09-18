bd54cbeca090f0d3a42017fb69b3e0cb180372cf
Hylke Middel <h.middel@st.hanze.nl>
2019-09-18 13:35:24 +0200

    Added comments to the scorpion energy minimization step

--- src/main/java/nl/bioinf/cawarmerdam/compound_evolver/model/pipeline/ScorpionEnergyMinimizationStep.java
+++ src/main/java/nl/bioinf/cawarmerdam/compound_evolver/model/pipeline/ScorpionEnergyMinimizationStep.java
@@ -1,6 +1,7 @@
 package nl.bioinf.cawarmerdam.compound_evolver.model.pipeline;
 
 import nl.bioinf.cawarmerdam.compound_evolver.model.Candidate;
+import org.apache.commons.io.IOUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -23,7 +24,7 @@ public class ScorpionEnergyMinimizationStep implements PipelineStep<Candidate, C
     private String scorpionExecutable;
     private Path anchorFilePath;
     /**
-     * Constructor for an energy minimization step.
+     * Constructor for the scorpion energy minimization step.
      *
      * @param receptor     The force field that should be chosen in minimization.
      * @param anchorFilePath The path of the file that holds the anchor.
@@ -36,15 +37,15 @@ public class ScorpionEnergyMinimizationStep implements PipelineStep<Candidate, C
     }
 
     /**
-     * Should at the very least set conformer scores and set the conformer file location
+     * Runs scorpion on the candidate
      * @param candidate The candidate to find the conformer scores for
-     * @return
-     * @throws PipelineException
+     * @return the candidate with updated parameters
+     * @throws PipelineException when a problem occurs with running scorpion
      */
     @Override
     public Candidate execute(Candidate candidate) throws PipelineException {
-        // TODO: Run scorpion here
         scorpion(candidate.getFixedConformersFile());
+        //scorpion output takes the form of "original name_scorp.sdf" where original_name is the original file name without the extension
         Path output = anchorFilePath.resolveSibling(anchorFilePath.toString().replaceAll(".sdf", "") + "_scorp.sdf");
         candidate.setMinimizationOutputFilePath(output);
         try {
@@ -55,13 +56,19 @@ public class ScorpionEnergyMinimizationStep implements PipelineStep<Candidate, C
         return candidate;
     }
 
+    /**
+     * Gets the conformer scores from the output of scorpion
+     * @param scorpionoutput the path to the scorpion output file
+     * @return a list of scores for each conformer, or positive infinity if the conformer has no score
+     * @throws IOException if reading the file fails
+     */
     private static List<Double> getConformerScores(Path scorpionoutput) throws IOException {
         ArrayList<Double> conformerScores = new ArrayList<>();
         List<String> l = Files.readAllLines(scorpionoutput);
         //Does the next line have a score? Used to read the scores without reading other random numbers
         boolean next_has_score = false;
 
-        //Did the block we were in contain a score? If not, we append Double.NEGATIVE_INFINITY, to guarantee it won't be the best one
+        //Did the block we were in contain a score? If not, we append Double.POSITIVE_INFINITY, to guarantee it won't be the best one
         boolean block_has_score = false;
         for (String line : l) {
             // The scores are on each line starting with 'Affinity:'
@@ -70,10 +77,11 @@ public class ScorpionEnergyMinimizationStep implements PipelineStep<Candidate, C
                 block_has_score = true;
             } else if (next_has_score) {
                 next_has_score = false;
-                conformerScores.add(Double.parseDouble(line));
+                //Higher scores are better in scorpion, but the rest of the pipeline assumes lower scores are better
+                conformerScores.add(-Double.parseDouble(line));
             } else if (line.startsWith("$$$$")) {
                 if (!block_has_score) {
-                    conformerScores.add(Double.NEGATIVE_INFINITY);
+                    conformerScores.add(Double.POSITIVE_INFINITY);
                 }
                 //New block, so the new block doesn't have a score yet
                 block_has_score = false;
@@ -87,6 +95,7 @@ public class ScorpionEnergyMinimizationStep implements PipelineStep<Candidate, C
         String line;
 
         try {
+            //The command to run
             ProcessBuilder builder = new ProcessBuilder(
                     scorpionExecutable,
                     "-p", String.valueOf(receptorFilePath),
@@ -105,12 +114,11 @@ public class ScorpionEnergyMinimizationStep implements PipelineStep<Candidate, C
             while ((stdInput.readLine()) != null) {
             }
 
-//            // read any errors from the attempted command
-//            String stdErrorMessage = IOUtils.toString(stdError);
-//            if (!stdErrorMessage.isEmpty()) {
-//                candidate.getPipelineLogger().warning(
-//                        String.format("Smina has written an error message:%n%s%n", stdErrorMessage));
-//            }
+            // read any errors from the attempted command
+            String stdErrorMessage = IOUtils.toString(stdError);
+            if (!stdErrorMessage.isEmpty()) {
+                System.out.println(String.format("Smina has written an error message:%n%s%n", stdErrorMessage));
+            }
 
         } catch (IOException e) {
 
