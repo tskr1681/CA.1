0f232156ee7e4a9eab28b851e887ff0fb2f142b7
LordPINE <middelhylke@gmail.com>
2020-05-27 10:30:40 +0200

    Add temporary similarity function in place of erroring ChemAxon function.

--- src/main/java/nl/bioinf/cawarmerdam/compound_evolver/util/SimilarityHelper.java
+++ src/main/java/nl/bioinf/cawarmerdam/compound_evolver/util/SimilarityHelper.java
@@ -1,24 +1,11 @@
 package nl.bioinf.cawarmerdam.compound_evolver.util;
 
-import chemaxon.descriptors.MDGeneratorException;
-import chemaxon.descriptors.PFParameters;
-import chemaxon.descriptors.PharmacophoreFingerprint;
 import chemaxon.struc.Molecule;
 
 public class SimilarityHelper {
 
     public static double similarity(Molecule m1, Molecule m2) {
-        //TODO analyse the results of this. Do they seem decent for our purposes?
-        PharmacophoreFingerprint firstFingerprint = new PharmacophoreFingerprint(new PFParameters());
-        PharmacophoreFingerprint secondFingerprint = new PharmacophoreFingerprint(new PFParameters());
-        // Try to get the tanimoto dissimilarity score
-        try {
-            firstFingerprint.generate(m1);
-            secondFingerprint.generate(m2);
-            return 1-firstFingerprint.getTanimoto(secondFingerprint);
-        } catch (MDGeneratorException e) {
-            // Return a dissimilarity of 1 if the fingerprints could not be generated
-            return 0;
-        }
+        //TODO make this an actual similarity function
+        return 1-(m1.getMass() - m2.getMass())/(m1.getMass()+m2.getMass());
     }
 }
