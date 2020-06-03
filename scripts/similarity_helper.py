5c6de08b83e168e1332f1149bcff8354a0b58791
LordPINE <middelhylke@gmail.com>
2020-06-03 12:53:14 +0200

    Hopefully solve memory usage issues by switching to generators instead of list comprehensions

--- scripts/similarity_helper.py
+++ scripts/similarity_helper.py
@@ -7,12 +7,17 @@ from rdkit.Chem.Fingerprints import FingerprintMols
 def main():
     reference = int(sys.argv[2])
     with open(sys.argv[1]) as smiles:
-        mols = [Chem.MolFromSmiles(mol) for mol in smiles]
-    fingerprints = [FingerprintMols.FingerprintMol(mol) if mol is not None else None for mol in mols]
-    similarities = [DataStructs.FingerprintSimilarity(fingerprints[reference], fingerprint) if fingerprint is not None else 0 for fingerprint in fingerprints]
+        smiles_list = list(smiles)
+        mols = (get_mol(mol) for mol in smiles_list)
+    reference = FingerprintMols.FingerprintMol(smiles_list[reference])
+    fingerprints = (FingerprintMols.FingerprintMol(mol) if mol is not None else None for mol in mols)
+    similarities = (DataStructs.FingerprintSimilarity(reference, fingerprint) if fingerprint is not None else 0 for fingerprint in fingerprints)
     for similarity in similarities:
         print(similarity)
 
+
+def get_mol(smiles):
+    Chem.MolFromSmiles(smiles)
 
 if __name__ == "__main__":
     main()
